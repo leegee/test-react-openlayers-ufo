0cc3693bb943125e24c6fa10beb6dfeea61f223d
Lee Goddard <leegee@gmail.com>
2024-03-14 16:05:21 +0100

    Panel

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react'; 
+import React from 'react';
 
 import './App.css';
 import OpenLayersMap from './Map';
+import Panel from './Panel';
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>Map</h1>
       <OpenLayersMap />
+      <Panel />
     </div>
   );
 }
