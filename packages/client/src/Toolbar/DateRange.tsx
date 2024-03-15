f0b78791b92c8511c96719ff0118b6f93f5b0f3b
Lee Goddard <leegee@gmail.com>
2024-03-15 02:42:18 +0100

    Remove slider, use input boxes

--- packages/client/src/Toolbar/DateRange.tsx
+++ packages/client/src/Toolbar/DateRange.tsx
@@ -1,44 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import Slider from 'rc-slider';
 
 import { MapDictionary } from '@ufo-monorepo-test/common-types/src';
 import { RootState } from '../redux/types';
 
-import 'rc-slider/assets/index.css';
 import './DateRange.css';
 
 const DateRange: React.FC = () => {
     const dictionary: MapDictionary | undefined = useSelector((state: RootState) => state.map.dictionary);
 
-    const [minValueSelected, setMinValueSelected] = useState<number>(1);
-    const [maxValueSelected, setMaxValueSelected] = useState<number>(2);
+    const [minYear, setMinYear] = useState<number | undefined>(undefined);
+    const [maxYear, setMaxYear] = useState<number | undefined>(undefined);
+    const [error, setError] = useState<string | undefined>(undefined);
 
     useEffect(() => {
-        if (dictionary && dictionary.datetime) {
-            setMinValueSelected(dictionary.datetime.min || 0);
-            setMaxValueSelected(dictionary.datetime.max || 2);
+        if (dictionary && dictionary.datetime && dictionary.datetime.min && dictionary.datetime.max) {
+            setMinYear(dictionary.datetime.min);
+            setMaxYear(dictionary.datetime.max);
         }
     }, [dictionary]);
 
-    const handleSliderChange = (values: number | number[]) => {
-        if (values instanceof Array) {
-            setMinValueSelected(values[0]);
-            setMaxValueSelected(values[1]);
+    const handleMinYearChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(event.target.value);
+        if (!isNaN(value) && (maxYear === undefined || value <= maxYear)) {
+            setMinYear(value);
+            setError(undefined);
+        } else {
+            setError('Minimum year must be less than or equal to maximum year');
+        }
+    };
+
+    const handleMaxYearChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(event.target.value);
+        if (!isNaN(value) && (minYear === undefined || value >= minYear)) {
+            setMaxYear(value);
+            setError(undefined);
+        } else {
+            setError('Maximum year must be greater than or equal to minimum year');
         }
     };
 
     return (
         <aside className='date-range'>
-            <span className='date from'>{minValueSelected === 1 ? 'N/A' : minValueSelected}</span>
-            <Slider
-                range={true}
-                min={minValueSelected}
-                max={maxValueSelected}
-                value={[minValueSelected, maxValueSelected]}
-                onChange={handleSliderChange}
+            <span className='grey calendar'>📅</span>
+            <input
+                type='number'
+                id='minYear'
+                name='minYear'
+                value={minYear === undefined ? '' : String(minYear).padStart(4, '0')}
+                onChange={handleMinYearChange}
             />
-            <span className='date to'>{maxValueSelected}</span>
+            -
+            <input
+                type='number'
+                id='maxYear'
+                name='maxYear'
+                value={maxYear === undefined ? '' : maxYear}
+                onChange={handleMaxYearChange}
+            />
+            {/* {error && <span className='error'>{error}</span>} */}
         </aside>
     );
 }
