86ec16d9f0a3e1b6e7f6bce23bf2c83f2102527f
Lee Goddard <leegee@gmail.com>
2024-03-15 14:43:27 +0100

    expose

--- packages/client/src/redux/store.ts
+++ packages/client/src/redux/store.ts
@@ -7,12 +7,12 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
-  unknown, 
+  unknown,
   Action<string>
-  >;
+>;
 
 export const store = configureStore({
   reducer: rootReducer,
 });
 
-
+export type AppDispatch = typeof store.dispatch;
