f9bede2c7212d0c0c9677dc939711d9935e5b497
Lee Goddard <leegee@gmail.com>
2024-03-15 14:18:11 +0100

    Highlight matches

--- packages/client/src/FeaturesTable.tsx
+++ packages/client/src/FeaturesTable.tsx
@@ -2,12 +2,14 @@
 import React, { useEffect, useState } from 'react';
 import { get } from 'react-intl-universal';
 import { useSelector } from 'react-redux';
+import { RootState } from './redux/types';
 
 import './FeatureTable.css';
 
 const FeatureTable: React.FC = () => {
     const featureCollection = useSelector((state: any) => state.map.featureCollection);
-    const [features, setFeatures] = useState<any[]>([]); // Change 'any' to your actual feature type
+    const [features, setFeatures] = useState<any[]>([]);
+    const { q } = useSelector((state: RootState) => state.map);
 
     useEffect(() => {
         if (featureCollection) {
@@ -18,6 +20,21 @@ const FeatureTable: React.FC = () => {
     if (!features || !features.length) {
         return (<p className='no_data'>{get('report.no_data')}</p>);
     }
+
+    const highlightText = (text: string) => {
+        if (!text) return;
+        if (typeof q === 'undefined' || q.trim() === '') {
+            return text;
+        }
+        const parts = text.split(new RegExp(`(${q})`, 'gi'));
+        return parts.map((part, index) =>
+            part.toLowerCase() === q.toLowerCase() ? (
+                <mark key={index}>{part}</mark>
+            ) : (
+                <React.Fragment key={index}>{part}</React.Fragment>
+            )
+        );
+    };
 
     return (
         <table className='feature-table'>
@@ -32,8 +49,8 @@ const FeatureTable: React.FC = () => {
                 {features.map((feature: any, index: number) => (
                     <tr key={index}>
                         <td>{feature.properties.datetime_original}</td>
-                        <td>{feature.properties.location_text}</td>
-                        <td>{feature.properties.report_text}</td>
+                        <td>{highlightText(feature.properties.location_text)}</td>
+                        <td>{highlightText(feature.properties.report_text)}</td>
                     </tr>
                 ))}
             </tbody>
