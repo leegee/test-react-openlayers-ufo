765f6886f09d5858e8f545c61d74cdc53772bca1
Lee Goddard <leegee@gmail.com>
2024-03-17 08:44:57 +0100

    Rationalise

--- packages/server-tiny/src/middleware/errors.ts
+++ packages/server-tiny/src/middleware/errors.ts
@@ -2,13 +2,25 @@
 import Koa from 'koa';
 
 export class CustomError {
-    constructor(
-        public action: string,
-        public details: any,
-        public status: number = 500,
-        public msg?: string | string[],
-        public error?: Error | string
-    ) { }
+    action: string;
+    details: any;
+    status?: number;
+    msg?: string | string[];
+    error?: Error | string;
+
+    constructor(args: {
+        action: string;
+        details: any;
+        status?: number;
+        msg?: string | string[];
+        error?: Error | string;
+    }) {
+        this.action = args.action;
+        this.details = args.details;
+        this.status = args.status || 500;
+        if (args.msg) this.msg = args.msg;
+        if (args.error) this.error = args.error;
+    }
 }
 
 export async function errorHandler(ctx: Koa.Context, next: Koa.Next) {
