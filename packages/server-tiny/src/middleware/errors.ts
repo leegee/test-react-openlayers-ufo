ea7ba5b08d068b51cba013b4124241b7f5b17be2
Lee Goddard <leegee@gmail.com>
2024-03-17 08:42:32 +0100

    Rationalise

--- packages/server-tiny/src/middleware/errors.ts
+++ packages/server-tiny/src/middleware/errors.ts
@@ -1,22 +1,14 @@
 // middleware/errors.ts - Error handling middleware
 import Koa from 'koa';
 
-export type CustomErrorType = {
-    action: string;
-    details: any;
-    status?: number;
-    msg?: string | string[];
-    error?: Error | string;
-}
-
-export class CustomError extends Error {
-    constructor(args: CustomErrorType) {
-        args.status = args.status || 500;
-        const message = JSON.stringify(args, null, 4);
-        super(message);
-        this.name = this.constructor.name;
-        Object.setPrototypeOf(this, CustomError.prototype);
-    }
+export class CustomError {
+    constructor(
+        public action: string,
+        public details: any,
+        public status: number = 500,
+        public msg?: string | string[],
+        public error?: Error | string
+    ) { }
 }
 
 export async function errorHandler(ctx: Koa.Context, next: Koa.Next) {
