8fdcb5bbd31b4cc8748c982143fc5af332a6fbce
Lee Goddard <leegee@gmail.com>
2024-03-19 12:48:50 +0100

    wip

---
+++ packages/client/src/Histogram.ts
@@ -0,0 +1,30 @@
+// import React from 'react';
+// import { Bar } from 'react-chartjs-2';
+
+// const Histogram = ({ features }) => {
+//   const datetimeValues = featureCollection.features.map(feature => new Date(feature.get('datetime')).getTime());
+
+//   const data = {
+//     labels: [], 
+//     datasets: [
+//       {
+//         label: 'Histogram',
+//         backgroundColor: 'rgba(75,192,192,0.4)',
+//         borderColor: 'rgba(75,192,192,1)',
+//         borderWidth: 1,
+//         hoverBackgroundColor: 'rgba(75,192,192,0.6)',
+//         hoverBorderColor: 'rgba(75,192,192,1)',
+//         data: datetimeValues,
+//       },
+//     ],
+//   };
+
+//   return (
+//     <div>
+//       <h2>Histogram</h2>
+//       <Bar data={data} />
+//     </div>
+//   );
+// };
+
+// export default Histogram;
