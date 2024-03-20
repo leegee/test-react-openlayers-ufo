cc7d53dd88ea651abcec9bf1777f0510b3eed170
Lee Goddard <leegee@gmail.com>
2024-03-20 15:13:35 +0100

    Removed vanilla setup

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-// import { FooComponent } from "@ufo-monorepo-test/client"
-// import { isEven } from "@ufo-monorepo-test/common-types"
-
-// console.log(isEven(4))
-// FooComponent()
-
-console.log('test');
