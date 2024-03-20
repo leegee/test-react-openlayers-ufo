7eb98d7f6fc75d4b1c9abf21bc7f3b381e40e84e
Lee Goddard <leegee@gmail.com>
2024-03-20 15:09:52 +0100

    Test fails but does run!

--- packages/client/jest.config.cjs
+++ packages/client/jest.config.cjs
@@ -6,4 +6,7 @@ module.exports = {
     extensionsToTreatAsEsm: [ ".ts" ],
     testRegex: '\\.test\\.tsx?$',
     testPathIgnorePatterns: [ '<rootDir>/node_modules/', '<rootDir>/build/', '\\.d\\.ts$' ],
+    moduleNameMapper: {
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+    }
 };
