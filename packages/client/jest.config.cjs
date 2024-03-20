4db293e6b5a0d9adc03030a3145f501e5b955bfe
Lee Goddard <leegee@gmail.com>
2024-03-20 15:08:41 +0100

    All but CSS testable

--- packages/client/jest.config.cjs
+++ packages/client/jest.config.cjs
@@ -3,7 +3,7 @@
 module.exports = {
     testEnvironment: 'jsdom',
     preset: 'ts-jest',
-    // transform: {
-    //     '^.+\\.jsx?$': 'babel-jest',
-    // },
+    extensionsToTreatAsEsm: [ ".ts" ],
+    testRegex: '\\.test\\.tsx?$',
+    testPathIgnorePatterns: [ '<rootDir>/node_modules/', '<rootDir>/build/', '\\.d\\.ts$' ],
 };
