3b6d268679b788e065961bb985417552c3b74c0a
Lee Goddard <leegee@gmail.com>
2024-03-25 13:57:03 +0100

    Update ESLint to prod recommendations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,12 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    // plugin:@typescript-eslint/stylistic-type-checked,
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/jsx-runtime',
   ],
   ignorePatterns: [ 'dist', '.eslintrc.cjs' ],
   parser: '@typescript-eslint/parser',
@@ -16,5 +20,11 @@ module.exports = {
       { 'react/jsx-uses-react': 'off' },
       { 'react/react-in-jsx-scope': 'off' }
     ],
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: [ './tsconfig.json', './tsconfig.node.json' ],
+    tsconfigRootDir: __dirname,
   },
 }
