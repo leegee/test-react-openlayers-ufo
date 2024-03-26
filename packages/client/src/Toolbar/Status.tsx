c249a37fbb28041798ee3dbb1c9a94743298b5dc
Lee Goddard <leegee@gmail.com>
2024-03-26 12:34:02 +0100

    Remove gradient

--- packages/client/src/Toolbar/Status.tsx
+++ packages/client/src/Toolbar/Status.tsx
@@ -16,36 +16,29 @@ const Panel: React.FC = () => {
     const clusterCount = useSelector(selectClusterCount);
     const nothingToShow = !clusterCount && !pointsCount;
     const showPoints = !clusterCount && pointsCount && pointsCount > 0;
-    let gradientBorder;
-
-    if (q) {
-        gradientBorder = {
-            borderImage: `linear-gradient(to right, hsl(${mapScoreToHue(0)}, 100%, 50%), hsl(${mapScoreToHue(1)}, 100%, 50%)) 1`,
-            borderStyle: 'solid',
-            borderWidth: '1pt',
-        };
-    }
 
     useEffect(() => void (0), [locale]);
 
     return (
-        <header className='report-ctrl component' style={gradientBorder}><span className='inner'>
-            {nothingToShow ? (
-                <>{get('panel.no_results')}</>
-            ) : showPoints ? (
-                <>
-                    {new Intl.NumberFormat(config.locale).format(pointsCount)}
-                    {' '}
-                    {get('panel.results_count')}
-                </>
-            ) : (
-                <>
-                    {new Intl.NumberFormat(config.locale).format(clusterCount)}
-                    {' '}
-                    {get('panel.cluster_count')}
-                </>
-            )}
-        </span></header>
+        <header className='report-ctrl component'>
+            <span className='inner'>
+                {nothingToShow ? (
+                    <>{get('panel.no_results')}</>
+                ) : showPoints ? (
+                    <>
+                        {new Intl.NumberFormat(config.locale).format(pointsCount)}
+                        {' '}
+                        {get('panel.results_count')}
+                    </>
+                ) : (
+                    <>
+                        {new Intl.NumberFormat(config.locale).format(clusterCount)}
+                        {' '}
+                        {get('panel.cluster_count')}
+                    </>
+                )}
+            </span>
+        </header>
     );
 };
 
