f3ce439a6fbfbdc5c744dd09c6968adeeb362065
Lee Goddard <leegee@gmail.com>
2024-03-31 20:21:30 +0200

    Vary heatmap display config by zoom level

--- packages/client/src/lib/HeatmapLayer.ts
+++ packages/client/src/lib/HeatmapLayer.ts
@@ -1,8 +1,8 @@
+import type { View } from 'ol';
 import GeoJSON from 'ol/format/GeoJSON';
 import VectorSource from 'ol/source/Vector';
 import { bbox } from "ol/loadingstrategy";
 import { Heatmap as HeatmapLayer } from 'ol/layer';
-
 import type { UfoFeatureCollection } from '../redux/mapSlice';
 
 const vectorSource = new VectorSource({
@@ -21,11 +21,32 @@ export const vectorLayer = new HeatmapLayer({
 
 vectorLayer.set('name', 'server-clusters-only');
 
-export function updateVectorLayer(featureCollection: UfoFeatureCollection) {
-    vectorSource.clear();
-    if (featureCollection.features !== null) {
-        vectorSource.addFeatures(new GeoJSON().readFeatures(featureCollection));
+export function updateVectorLayer(featureCollection: UfoFeatureCollection|null) {
+    if (featureCollection) {
+        if (featureCollection.features) {
+            vectorSource.clear();
+            vectorSource.addFeatures(new GeoJSON().readFeatures(featureCollection));
+            vectorSource.changed();
+            console.debug("Number of features added:", vectorSource.getFeatures().length);
+        }
+    } else {
+        console.debug('No features to add');
     }
-    vectorSource.changed();
-    console.debug("Number of features added:", vectorSource.getFeatures().length);
+} 
+
+export function setupHeatmapListeners(map){
+    map.on('moveend', () => updateLayerProperties(map) );
+}
+
+function updateLayerProperties(map) {
+    if (map) {
+        const view = map.getView() as View | undefined;
+        const zoom: number = view.getZoom();
+        const newRadius = zoom >= 5 ? 14 : 5; 
+        const newBlur = zoom >= 5 ? 12 : 7; 
+        console.log(zoom);
+        
+        vectorLayer.setBlur(newBlur);
+        vectorLayer.setRadius(newRadius);
+    }
 }
