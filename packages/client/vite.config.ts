77af03b41513f0ec0f7341df8bdafb033c00b6f9
Lee Goddard <leegee@gmail.com>
2024-10-23 13:29:12 +0200

    Manifest

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -68,15 +68,29 @@ export default defineConfig({
         background_color: '#ffffff',
         icons: [
           {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            "src": "icons/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
           },
           {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            "src": "icons/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
           },
+          {
+            "src": "icons/manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "icons/manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
         ],
       },
 
