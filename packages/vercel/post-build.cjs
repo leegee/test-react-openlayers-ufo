480ea61c880b2931c979e9d9072182eef7e48f5f
Lee Goddard <leegee@gmail.com>
2024-10-29 16:06:13 +0100

    Try adding vercel function config, copying built packages/vercel to api/. Odd.

---
+++ packages/vercel/post-build.cjs
@@ -0,0 +1,22 @@
+const fs = require( 'fs' );
+const path = require( 'path' );
+
+if ( !process.cwd().includes( path.join( 'packages', 'vercel' ) ) ) {
+    console.error( "This script should be run from packages/vercel. Exiting as run in", process.cwd() );
+    process.exit( 1 );
+}
+
+const sourceDir = path.join( __dirname, 'build' );
+const targetDir = path.join( __dirname, '..', '..', 'api' );
+
+if ( !fs.existsSync( targetDir ) ) {
+    fs.mkdirSync( targetDir, { recursive: true } );
+}
+
+fs.readdirSync( sourceDir ).forEach( file => {
+    const srcFile = path.join( sourceDir, file );
+    const destFile = path.join( targetDir, file );
+
+    fs.copyFileSync( srcFile, destFile );
+    console.log( `Copied ${ file } to ${ targetDir }` );
+} );
